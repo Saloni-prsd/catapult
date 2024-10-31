f32a0b7ddc2a1a31f377d6f5f5de3cb0169c4f74
Saloni <44397730+Saloni-prsd@users.noreply.github.com>
2024-10-31 20:05:44 +0530

    Update appengine_dev_server.py

--- catapult_build/appengine_dev_server.py
+++ catapult_build/appengine_dev_server.py
@@ -3,9 +3,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-import subprocess
-import sys
 
 from catapult_build import temp_deployment_dir
 
